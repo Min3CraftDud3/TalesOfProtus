df34793e9247c64a2a3083d5fdec39362ff4c544
VaporMax <techadmin@vapormaxsouth.com>
2017-01-25 16:07:37 -0500

    Added Camera, 3D model loader, Started optimising render engine

--- src/com/SinfulPixel/top/Main.java
+++ src/com/SinfulPixel/top/Main.java
@@ -1,33 +1,48 @@
 package com.SinfulPixel.top;
 
+import com.SinfulPixel.top.Entities.Camera;
+import com.SinfulPixel.top.Entities.Entity;
+import com.SinfulPixel.top.Entities.Light;
 import com.SinfulPixel.top.Models.TexturedModel;
 import com.SinfulPixel.top.RenderEngine.DisplayManager;
 import com.SinfulPixel.top.RenderEngine.Loader;
 import com.SinfulPixel.top.Models.RawModel;
+import com.SinfulPixel.top.RenderEngine.OBJLoader;
 import com.SinfulPixel.top.RenderEngine.Renderer;
 import com.SinfulPixel.top.Shaders.StaticShader;
 import com.SinfulPixel.top.Textures.ModelTexture;
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
 public class Main {
     public Main() {
         DisplayManager.createDisplay();
         Loader loader = new Loader();
-        Renderer renderer = new Renderer();
         StaticShader shader = new StaticShader();
-        float[] vertices = {-0.5f,0.5f,0f,
+        Renderer renderer = new Renderer(shader);
+        /*float[] vertices = {-0.5f,0.5f,0f,
                             -0.5f,-0.5f,0f,
                             0.5f,-0.5f,0f,
                             0.5f,0.5f,0f};
         int[] indices = {0,1,3,3,1,2};
-        float[] textureCoords = {0,0,0,1,1,1,1,0};
-        RawModel model = loader.loadToVAO(vertices,textureCoords,indices);
-        ModelTexture texture = new ModelTexture(loader.loadTexture("flag"));
-        TexturedModel texturedModel = new TexturedModel(model,texture);
+        float[] textureCoords = {0,0,0,1,1,1,1,0};*/
+        RawModel model = OBJLoader.loadObjModel("dragon",loader);
+        TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("white")));
+        ModelTexture texture = staticModel.getTexture();
+        texture.setShineDamper(10);
+        texture.setReflectivity(1);
+        Camera camera = new Camera();
+        Entity entity = new Entity(staticModel,new Vector3f(0,0,-50),0,0,0,1);
+        Light light = new Light(new Vector3f(0,0,-20),new Vector3f(1,1,1));
         while(!Display.isCloseRequested()){
+            //entity.increasePosition(0,0,-0.05f);
+            entity.increaseRotation(0,1,0);
+            camera.move();
             renderer.prepare();
             shader.start();
-            renderer.render(texturedModel);
+            shader.loadLight(light);
+            shader.loadViewMatrix(camera);
+            renderer.render(entity,shader);
             shader.stop();
             DisplayManager.updateDisplay();
         }
--- src/com/SinfulPixel/top/Shaders/StaticShader.java
+++ src/com/SinfulPixel/top/Shaders/StaticShader.java
@@ -1,5 +1,10 @@
 package com.SinfulPixel.top.Shaders;
 
+import com.SinfulPixel.top.Entities.Camera;
+import com.SinfulPixel.top.Entities.Light;
+import com.SinfulPixel.top.ToolBox.Maths;
+import org.lwjgl.util.vector.Matrix4f;
+
 /**
  * Created by Vapor on 1/24/2017.
  */
@@ -8,16 +13,57 @@ public class StaticShader extends ShaderProgram{
     private static final String VERTEX_FILE = "src/com/SinfulPixel/top/Shaders/vertexShader.txt";
     private static final String FRAGMENT_FILE = "src/com/SinfulPixel/top/Shaders/fragmentShader.txt";
 
+    private int location_transformationMatrix;
+    private int location_projectionMatrix;
+    private int location_viewMatrix;
+    private int location_lightPosition;
+    private int location_lightColor;
+    private int location_reflectivity;
+    private int location_shineDamper;
+
     public StaticShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
+    }
+
+    @Override
+    protected void getAllUniformLocations() {
+        location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+        location_projectionMatrix = super.getUniformLocation("projectionMatrix");
+        location_viewMatrix = super.getUniformLocation("viewMatrix");
+        location_lightPosition = super.getUniformLocation("lightPosition");
+        location_lightColor = super.getUniformLocation("lightColor");
+        location_reflectivity = super.getUniformLocation("reflectivity");
+        location_shineDamper = super.getUniformLocation("shineDamper");
+    }
+
+    public void loadShineVars(float damper,float reflect){
+        super.loadFloat(location_shineDamper,damper);
+        super.loadFloat(location_reflectivity,reflect);
     }
 
     @Override
     protected void bindAttributes() {
         super.bindAttribute(0, "position");
         super.bindAttribute(1,"textureCoords");
+        super.bindAttribute(2,"normal");
     }
 
+    public void loadTransformationMatrix(Matrix4f matrix){
+        super.loadMatrix(location_transformationMatrix,matrix);
+    }
 
+    public void loadLight(Light light){
+        super.loadVector(location_lightPosition,light.getPosition());
+        super.loadVector(location_lightColor,light.getColor());
+    }
+
+    public void loadViewMatrix(Camera camera){
+        Matrix4f viewMatrix = Maths.createViewMatrix(camera);
+        super.loadMatrix(location_viewMatrix,viewMatrix);
+    }
+
+    public void loadProjectionMatrix(Matrix4f projection){
+        super.loadMatrix(location_projectionMatrix,projection);
+    }
 
 }
